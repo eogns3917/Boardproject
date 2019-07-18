6fde84f8ebe2c5bcb9bb5b1f472d982df96c76dd
Dae Hun Sung <eogns3917@Naver.com>
2019-07-18 14:25:04 +0900

    Boardheroku

--- Board/settings.py
+++ Board/settings.py
@@ -10,10 +10,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u*ikqhgu=b604q*mw-*imy_oa_5chqmq(&-2w3wt74a0-0qiz3'
+
+#SECRET_KEY = 'u*ikqhgu=b604q*mw-*imy_oa_5chqmq(&-2w3wt74a0-0qiz3'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
